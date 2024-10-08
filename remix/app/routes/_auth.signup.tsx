2ab2aa47a45b4c88b639864002441613a28d134b
Katsushi OUGI <ougi@to-r.net>
2024-10-08 23:30:36 +0900

    Cognitoでサインアップするとこまで

--- remix/app/routes/_auth.signup.tsx
+++ remix/app/routes/_auth.signup.tsx
@@ -1,5 +1,9 @@
-import type { MetaFunction } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
+import { Form, json, useActionData } from "@remix-run/react";
+import {
+  SignUpCommand,
+  CognitoIdentityProviderClient,
+} from "@aws-sdk/client-cognito-identity-provider";
 
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
@@ -9,16 +13,59 @@ import {
   CardTitle,
   CardDescription,
   CardContent,
-  CardFooter,
 } from "~/components/ui/card";
 import { Button } from "~/components/ui/button";
 import { Alert, AlertDescription } from "~/components/ui/alert";
+import { COGNITO_CONFIG } from "~/config/cognito.server";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Todo App" }];
 };
 
+const cognitoClient = new CognitoIdentityProviderClient({
+  region: COGNITO_CONFIG.REGION,
+});
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  try {
+    const command = new SignUpCommand({
+      ClientId: COGNITO_CONFIG.CLIENT_ID,
+      Username: email,
+      Password: password,
+      UserAttributes: [
+        {
+          Name: "email",
+          Value: email,
+        },
+        {
+          Name: "name",
+          Value: name,
+        },
+      ],
+    });
+    await cognitoClient.send(command);
+    return json({ success: true, error: null });
+  } catch (error) {
+    console.error("Error during signup:", error);
+    let errorMessage = "サインアップに失敗しました。もう一度お試しください。";
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+    return json({
+      error: errorMessage,
+      success: false,
+    });
+  }
+};
+
 export default function Index() {
+  const actionData = useActionData<typeof action>();
+
   return (
     <>
       <div className="flex justify-center items-center bg-gray-100">
@@ -31,54 +78,32 @@ export default function Index() {
           </CardHeader>
           <CardContent>
             <Form method="post" className="space-y-4">
-              <Alert variant="destructive">
-                <AlertDescription>
-                  <ul className="list-disc pl-4">
-                    <li>エラーです</li>
-                    {/* {Object.values(actionData.errors).map((error, index) => (
-                    <li key={index}>{error}</li>
-                  ))} */}
-                  </ul>
-                </AlertDescription>
-              </Alert>
+              {actionData?.error && (
+                <Alert variant="destructive">
+                  <AlertDescription>{actionData?.error}</AlertDescription>
+                </Alert>
+              )}
               <div className="space-y-2">
-                <Label htmlFor="username">ユーザー名</Label>
-                <Input
-                  id="username"
-                  name="username"
-                  type="text"
-                  required
-                  aria-invalid
-                  aria-describedby="username-error"
-                />
-                <p className="text-sm text-red-500" id="username-error">
-                  入力してください
-                </p>
+                <Label htmlFor="name" aria-required>
+                  ユーザー名
+                </Label>
+                <Input id="name" name="name" required />
               </div>
               <div className="space-y-2">
-                <Label htmlFor="email">メールアドレス</Label>
+                <Label htmlFor="email" aria-required>
+                  メールアドレス
+                </Label>
                 <Input id="email" name="email" type="email" required />
               </div>
               <div className="space-y-2">
                 <Label htmlFor="password">パスワード</Label>
                 <Input id="password" name="password" type="password" required />
               </div>
-              <div className="space-y-2">
-                <Label htmlFor="confirmPassword">パスワード（確認）</Label>
-                <Input
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  type="password"
-                  required
-                />
-              </div>
+              <Button type="submit" className="w-full">
+                アカウント作成
+              </Button>
             </Form>
           </CardContent>
-          <CardFooter>
-            <Button type="submit" className="w-full">
-              アカウント作成
-            </Button>
-          </CardFooter>
         </Card>
       </div>
     </>
